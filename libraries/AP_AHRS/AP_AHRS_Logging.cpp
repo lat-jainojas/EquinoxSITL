ArduPilot/ardupilot

AP_AHRS: avoid use of location .alt field

libraries/AP_AHRS/AP_AHRS_Logging.cpp
@@ -18,13 +18,17 @@ void AP_AHRS::Write_AHRS2() const
     if (!get_secondary_attitude(euler) || !get_secondary_position(loc) || !get_secondary_quaternion(quat)) {
         return;
     }
+    float alt;
+    if (!loc.initialised() || !loc.get_alt_m(Location::AltFrame::ABSOLUTE, alt)) {
+        alt = AP::logger().quiet_nanf();
+    }
     const struct log_AHRS pkt{
         LOG_PACKET_HEADER_INIT(LOG_AHR2_MSG),
         time_us : AP_HAL::micros64(),
         roll  : (int16_t)(degrees(euler.x)*100),
         pitch : (int16_t)(degrees(euler.y)*100),
         yaw   : (uint16_t)(wrap_360_cd(degrees(euler.z)*100)),
-        alt   : loc.alt*1.0e-2f,
+        alt   : alt,
         lat   : loc.lat,
         lng   : loc.lng,
         q1    : quat.q1,
@@ -67,13 +71,17 @@ void AP_AHRS::Write_Attitude(const Vector3f &targets) const
 
 void AP_AHRS::Write_Origin(LogOriginType origin_type, const Location &loc) const
 {
+    int32_t alt_cm;
+    if (!loc.initialised() || !loc.get_alt_cm(Location::AltFrame::ABSOLUTE, alt_cm)) {
+        alt_cm = 0;
+    }
     const struct log_ORGN pkt{
         LOG_PACKET_HEADER_INIT(LOG_ORGN_MSG),
         time_us     : AP_HAL::micros64(),
         origin_type : (uint8_t)origin_type,
         latitude    : loc.lat,
         longitude   : loc.lng,
-        altitude    : loc.alt
+        altitude    : alt_cm
     };
     AP::logger().WriteBlock(&pkt, sizeof(pkt));
 }
@@ -85,16 +93,27 @@ void AP_AHRS::Write_POS() const
     if (!get_location(loc)) {
         return;
     }
-    float home, origin;
-    AP::ahrs().get_relative_position_D_home(home);
+    const auto nanf = AP::logger().quiet_nanf();
+    float home_pos_down;
+    if (!AP::ahrs().get_relative_position_D_home(home_pos_down)) {
+        home_pos_down = nanf;
+    }
+    float origin_pos_down;
+    if (!get_relative_position_D_origin(origin_pos_down)) {
+        origin_pos_down = nanf;
+    }
+    float alt;
+    if (!loc.get_alt_m(Location::AltFrame::ABSOLUTE, alt)) {
+        alt = nanf;
+    }
     const struct log_POS pkt{
         LOG_PACKET_HEADER_INIT(LOG_POS_MSG),
         time_us        : AP_HAL::micros64(),
         lat            : loc.lat,
         lng            : loc.lng,
-        alt            : loc.alt*1.0e-2f,
-        rel_home_alt   : -home,
-        rel_origin_alt : get_relative_position_D_origin(origin) ? -origin : AP::logger().quiet_nanf(),
+        alt            : alt,
+        rel_home_alt   : -home_pos_down,   // down->up
+        rel_origin_alt : -origin_pos_down, // down->up
     };
     AP::logger().WriteBlock(&pkt, sizeof(pkt));
 }

lat-jainojas/EquinoxSITL

libraries/AP_AHRS/AP_AHRS_Logging.cpp
@@ -18,13 +18,17 @@ void AP_AHRS::Write_AHRS2() const
     if (!get_secondary_attitude(euler) || !get_secondary_position(loc) || !get_secondary_quaternion(quat)) {
         return;
     }
+    float alt;
+    if (!loc.initialised() || !loc.get_alt_m(Location::AltFrame::ABSOLUTE, alt)) {
+        alt = AP::logger().quiet_nanf();
+    }
     const struct log_AHRS pkt{
         LOG_PACKET_HEADER_INIT(LOG_AHR2_MSG),
         time_us : AP_HAL::micros64(),
         roll  : (int16_t)(degrees(euler.x)*100),
         pitch : (int16_t)(degrees(euler.y)*100),
         yaw   : (uint16_t)(wrap_360_cd(degrees(euler.z)*100)),
-        alt   : loc.alt*1.0e-2f,
+        alt   : alt,
         lat   : loc.lat,
         lng   : loc.lng,
         q1    : quat.q1,
@@ -67,13 +71,17 @@ void AP_AHRS::Write_Attitude(const Vector3f &targets) const
 
 void AP_AHRS::Write_Origin(LogOriginType origin_type, const Location &loc) const
 {
+    int32_t alt_cm;
+    if (!loc.initialised() || !loc.get_alt_cm(Location::AltFrame::ABSOLUTE, alt_cm)) {
+        alt_cm = 0;
+    }
     const struct log_ORGN pkt{
         LOG_PACKET_HEADER_INIT(LOG_ORGN_MSG),
         time_us     : AP_HAL::micros64(),
         origin_type : (uint8_t)origin_type,
         latitude    : loc.lat,
         longitude   : loc.lng,
-        altitude    : loc.alt
+        altitude    : alt_cm
     };
     AP::logger().WriteBlock(&pkt, sizeof(pkt));
 }
@@ -85,16 +93,28 @@ void AP_AHRS::Write_POS() const
     if (!get_location(loc)) {
         return;
     }
-    float home, origin;
+    float home;
     AP::ahrs().get_relative_position_D_home(home);
+
+    const auto nanf = AP::logger().quiet_nanf();
+    float origin_pos_up;
+    if (get_relative_position_D_origin(origin_pos_up)) {
+        origin_pos_up *= -1;  // down -> up
+    } else {
+        origin_pos_up = nanf;
+    }
+    float alt;
+    if (!loc.get_alt_m(Location::AltFrame::ABSOLUTE, alt)) {
+        alt = nanf;
+    }
     const struct log_POS pkt{
         LOG_PACKET_HEADER_INIT(LOG_POS_MSG),
         time_us        : AP_HAL::micros64(),
         lat            : loc.lat,
         lng            : loc.lng,
-        alt            : loc.alt*1.0e-2f,
+        alt            : alt,
         rel_home_alt   : -home,
-        rel_origin_alt : get_relative_position_D_origin(origin) ? -origin : AP::logger().quiet_nanf(),
+        rel_origin_alt : origin_pos_up,
     };
     AP::logger().WriteBlock(&pkt, sizeof(pkt));
 }