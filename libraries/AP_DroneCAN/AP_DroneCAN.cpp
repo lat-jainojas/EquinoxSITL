ArduPilot/ardupilot

AP_DroneCAN: servo/esc don't output zero pwm

libraries/AP_DroneCAN/AP_DroneCAN.cpp
@@ -930,17 +930,23 @@ void AP_DroneCAN::SRV_push_servos()
 {
     WITH_SEMAPHORE(SRV_sem);
 
+    uint32_t none_zero_channels = 0;
     for (uint8_t i = 0; i < DRONECAN_SRV_NUMBER; i++) {
         // Check if this channels has any function assigned
         if (SRV_Channels::channel_function(i) >= SRV_Channel::k_none) {
             _SRV_conf[i].pulse = SRV_Channels::srv_channel(i)->get_output_pwm();
             _SRV_conf[i].esc_pending = true;
             _SRV_conf[i].servo_pending = true;
+
+            // Flag channels which are none zero
+            if (_SRV_conf[i].pulse > 0) {
+                none_zero_channels |= 1U << i;
+            }
         }
     }
 
-    uint32_t servo_armed_mask = _servo_bm;
-    uint32_t esc_armed_mask = _esc_bm;
+    uint32_t servo_armed_mask = _servo_bm & none_zero_channels;
+    uint32_t esc_armed_mask = _esc_bm & none_zero_channels;
     const bool safety_off = hal.util->safety_switch_state() != AP_HAL::Util::SAFETY_DISARMED;
     if (!safety_off) {
         AP_BoardConfig *boardconfig = AP_BoardConfig::get_singleton();

lat-jainojas/EquinoxSITL

libraries/AP_DroneCAN/AP_DroneCAN.cpp
@@ -930,17 +930,23 @@ void AP_DroneCAN::SRV_push_servos()
 {
     WITH_SEMAPHORE(SRV_sem);
 
+    uint32_t non_zero_channels = 0;
     for (uint8_t i = 0; i < DRONECAN_SRV_NUMBER; i++) {
         // Check if this channels has any function assigned
         if (SRV_Channels::channel_function(i) >= SRV_Channel::k_none) {
             _SRV_conf[i].pulse = SRV_Channels::srv_channel(i)->get_output_pwm();
             _SRV_conf[i].esc_pending = true;
             _SRV_conf[i].servo_pending = true;
+
+            // Flag channels which are non zero
+            if (_SRV_conf[i].pulse > 0) {
+                non_zero_channels |= 1U << i;
+            }
         }
     }
 
-    uint32_t servo_armed_mask = _servo_bm;
-    uint32_t esc_armed_mask = _esc_bm;
+    uint32_t servo_armed_mask = _servo_bm & non_zero_channels;
+    uint32_t esc_armed_mask = _esc_bm & non_zero_channels;
     const bool safety_off = hal.util->safety_switch_state() != AP_HAL::Util::SAFETY_DISARMED;
     if (!safety_off) {
         AP_BoardConfig *boardconfig = AP_BoardConfig::get_singleton();