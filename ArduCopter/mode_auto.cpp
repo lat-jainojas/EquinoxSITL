ArduPilot/ardupilot

Copter: remove most of the _cd() functions

ArduCopter/mode_auto.cpp
@@ -865,15 +865,15 @@ float ModeAuto::wp_distance_m() const
     }
 }
 
-int32_t ModeAuto::wp_bearing() const
+float ModeAuto::wp_bearing_deg() const
 {
     switch (_mode) {
     case SubMode::CIRCLE:
-        return copter.circle_nav->get_bearing_to_target_cd();
+        return degrees(copter.circle_nav->get_bearing_to_target_rad());
     case SubMode::WP:
     case SubMode::CIRCLE_MOVE_TO_EDGE:
     default:
-        return wp_nav->get_wp_bearing_to_destination_cd();
+        return degrees(wp_nav->get_bearing_to_target_rad());
     }
 }
 

ArduCopter/mode_guided.cpp
@@ -1128,15 +1128,15 @@ float ModeGuided::wp_distance_m() const
     }
 }
 
-int32_t ModeGuided::wp_bearing() const
+float ModeGuided::wp_bearing_deg() const
 {
     switch(guided_mode) {
     case SubMode::WP:
-        return wp_nav->get_wp_bearing_to_destination_cd();
+        return degrees(wp_nav->get_bearing_to_target_rad());
     case SubMode::Pos:
-        return get_bearing_cd(pos_control->get_pos_estimate_NEU_cm().xy().tofloat(), guided_pos_target_cm.xy().tofloat());
+        return degrees(get_bearing_rad(pos_control->get_pos_estimate_NEU_cm().xy().tofloat(), guided_pos_target_cm.xy().tofloat()));
     case SubMode::PosVelAccel:
-        return pos_control->get_bearing_to_target_cd();
+        return degrees(pos_control->get_bearing_to_target_rad());
     case SubMode::TakeOff:
     case SubMode::Accel:
     case SubMode::VelAccel:

ArduCopter/mode_rtl.cpp
@@ -536,9 +536,9 @@ float ModeRTL::wp_distance_m() const
     return wp_nav->get_wp_distance_to_destination_cm() * 0.01f;
 }
 
-int32_t ModeRTL::wp_bearing() const
+float ModeRTL::wp_bearing_deg() const
 {
-    return wp_nav->get_wp_bearing_to_destination_cd();
+    return degrees(wp_nav->get_bearing_to_target_rad());
 }
 
 // returns true if pilot's yaw input should be used to adjust vehicle's heading

ArduCopter/mode_smart_rtl.cpp
@@ -201,9 +201,9 @@ float ModeSmartRTL::wp_distance_m() const
     return wp_nav->get_wp_distance_to_destination_cm() * 0.01f;
 }
 
-int32_t ModeSmartRTL::wp_bearing() const
+float ModeSmartRTL::wp_bearing_deg() const
 {
-    return wp_nav->get_wp_bearing_to_destination_cd();
+    return degrees(wp_nav->get_bearing_to_target_rad());
 }
 
 bool ModeSmartRTL::use_pilot_yaw() const

lat-jainojas/EquinoxSITL

ArduCopter/mode_auto.cpp
@@ -865,15 +865,15 @@ float ModeAuto::wp_distance_m() const
     }
 }
 
-int32_t ModeAuto::wp_bearing() const
+float ModeAuto::wp_bearing_deg() const
 {
     switch (_mode) {
     case SubMode::CIRCLE:
-        return copter.circle_nav->get_bearing_to_target_cd();
+        return degrees(copter.circle_nav->get_bearing_to_target_rad());
     case SubMode::WP:
     case SubMode::CIRCLE_MOVE_TO_EDGE:
     default:
-        return wp_nav->get_wp_bearing_to_destination_cd();
+        return degrees(wp_nav->get_wp_bearing_to_destination_rad());
     }
 }
 

ArduCopter/mode_guided.cpp
@@ -1128,15 +1128,15 @@ float ModeGuided::wp_distance_m() const
     }
 }
 
-int32_t ModeGuided::wp_bearing() const
+float ModeGuided::wp_bearing_deg() const
 {
     switch(guided_mode) {
     case SubMode::WP:
-        return wp_nav->get_wp_bearing_to_destination_cd();
+        return degrees(wp_nav->get_wp_bearing_to_destination_rad());
     case SubMode::Pos:
-        return get_bearing_cd(pos_control->get_pos_estimate_NEU_cm().xy().tofloat(), guided_pos_target_cm.xy().tofloat());
+        return degrees(get_bearing_rad(pos_control->get_pos_estimate_NEU_cm().xy().tofloat(), guided_pos_target_cm.xy().tofloat()));
     case SubMode::PosVelAccel:
-        return pos_control->get_bearing_to_target_cd();
+        return degrees(pos_control->get_bearing_to_target_rad());
     case SubMode::TakeOff:
     case SubMode::Accel:
     case SubMode::VelAccel:

ArduCopter/mode_rtl.cpp
@@ -536,9 +536,9 @@ float ModeRTL::wp_distance_m() const
     return wp_nav->get_wp_distance_to_destination_cm() * 0.01f;
 }
 
-int32_t ModeRTL::wp_bearing() const
+float ModeRTL::wp_bearing_deg() const
 {
-    return wp_nav->get_wp_bearing_to_destination_cd();
+    return degrees(wp_nav->get_wp_bearing_to_destination_rad());
 }
 
 // returns true if pilot's yaw input should be used to adjust vehicle's heading

ArduCopter/mode_smart_rtl.cpp
@@ -201,9 +201,9 @@ float ModeSmartRTL::wp_distance_m() const
     return wp_nav->get_wp_distance_to_destination_cm() * 0.01f;
 }
 
-int32_t ModeSmartRTL::wp_bearing() const
+float ModeSmartRTL::wp_bearing_deg() const
 {
-    return wp_nav->get_wp_bearing_to_destination_cd();
+    return degrees(wp_nav->get_wp_bearing_to_destination_rad());
 }
 
 bool ModeSmartRTL::use_pilot_yaw() const